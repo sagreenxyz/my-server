8ac03bd29bb2803e8542a3fbbb0578d2938ca344
Scott Green <sagreenxyz@gmail.com>
2022-02-22 13:59:38 -0600

    With partial content (206) header

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let server = http.createServer((req, res) => {
-    res.write('<h1 style="color: blue">Hello FSD!</h1>');
+    res.writeHeader(206);
+    res.write('<h1>Bark!</h1><img src="https://httpstatusdogs.com/img/206.jpg" alt="206">');
     res.end();
 });
 
